b80b5e8cb7bf1ba9256cdcb880bd221ab1a2bef7
Jeremy Benoist <jeremy.benoist@gmail.com>
2015-08-21 20:24:08 +0200

    Implement SafeCurl
    Related to #7

---
+++ tests/Ring/Client/SafeCurlHandlerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Graby\Tests\Ring\Client;
+
+require __DIR__.'/../../../vendor/guzzlehttp/ringphp/tests/Client/Server.php';
+
+use GuzzleHttp\Tests\Ring\Client\Server;
+use Graby\Ring\Client\SafeCurlHandler;
+use GuzzleHttp\Ring\Client\CurlHandler;
+
+class SafeCurlHandlerTest extends \PHPUnit_Framework_TestCase
+{
+    protected function setUp()
+    {
+        if (!function_exists('curl_reset')) {
+            $this->markTestSkipped('curl_reset() is not available');
+        }
+    }
+
+    protected function getHandler($factory = null, $options = [])
+    {
+        return new SafeCurlHandler($options);
+    }
+
+    public function testCanSetMaxHandles()
+    {
+        $a = new SafeCurlHandler(['max_handles' => 10]);
+        $this->assertEquals(10, $this->readAttribute($a, 'maxHandles'));
+    }
+
+    public function testCreatesCurlErrors()
+    {
+        $handler = new SafeCurlHandler();
+        $response = $handler([
+            'http_method' => 'GET',
+            'uri' => '/',
+            'headers' => ['host' => ['localhost:123']],
+            'client' => ['timeout' => 0.001, 'connect_timeout' => 0.001],
+        ]);
+        $this->assertNull($response['status']);
+        $this->assertNull($response['reason']);
+        $this->assertEquals([], $response['headers']);
+        $this->assertInstanceOf(
+            'GuzzleHttp\Ring\Exception\RingException',
+            $response['error']
+        );
+
+        $this->assertEquals("Provided port '123' doesn't match whitelisted values: 80, 443, 8080", $response['error']->getMessage());
+    }
+
+    public function testReusesHandles()
+    {
+        Server::flush();
+        $response = ['status' => 200];
+        Server::enqueue([$response, $response]);
+        $a = new SafeCurlHandler();
+        $request = [
+            'http_method' => 'GET',
+            'headers'     => ['host' => [Server::$host]],
+        ];
+        $a($request);
+        $a($request);
+    }
+}
