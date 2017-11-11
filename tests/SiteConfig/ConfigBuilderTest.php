1f7db4f6de5c8ffda5ee7e8bf0d0c0d0a24650eb
Kevin Decherf <kevin@kdecherf.com>
2017-11-11 21:14:54 +0100

    Add some tests for strip_attr
    Signed-off-by: Kevin Decherf <kevin@kdecherf.com>

--- tests/SiteConfig/ConfigBuilderTest.php
+++ tests/SiteConfig/ConfigBuilderTest.php
@@ -36,6 +36,8 @@ class ConfigBuilderTest extends \PHPUnit_Framework_TestCase
             'replace_string(toto): titi',
             'http_header(user-agent): my-user-agent',
             'http_header(referer): http://idontl.ie',
+            'strip_attr: @class',
+            'strip_attr: @style',
         ]);
 
         $configExpected = new SiteConfig();
@@ -49,6 +51,7 @@ class ConfigBuilderTest extends \PHPUnit_Framework_TestCase
             'referer' => 'http://idontl.ie',
         ];
         $configExpected->date = ['foo'];
+        $configExpected->strip_attr = ['@class','@style'];
 
         $this->assertEquals($configExpected, $configActual);
 
