1d2de31d6abceac0db9783d243bbe5e19cd765d9
Jeremy Benoist <jeremy.benoist@gmail.com>
2022-08-29 11:20:38 +0200

    Replace `true/punycode` by `symfony/polyfill-intl-idn`
    Because it's no more maintained.
    
    (cherry picked from commit 06394db1333b6b716adb9f4f5a80c9798d04e825)

--- src/Graby.php
+++ src/Graby.php
@@ -19,7 +19,6 @@ use Psr\Log\NullLogger;
 use Readability\Readability;
 use Smalot\PdfParser\Parser as PdfParser;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use TrueBV\Punycode;
 
 /**
  * @todo add proxy
@@ -39,7 +38,6 @@ class Graby
 
     /** @var ConfigBuilder */
     private $configBuilder;
-    private $punycode;
 
     private $imgNoReferrer = false;
     private $prefetchedContent;
@@ -118,8 +116,6 @@ class Graby
             $this->config['http_client'],
             $this->logger
         );
-
-        $this->punycode = new Punycode();
     }
 
     /**
@@ -495,7 +491,13 @@ class Graby
         $uri = new Uri((string) $url);
 
         if (preg_match('/[\x80-\xff]/', $uri->getHost())) {
-            $uri = $uri->withHost($this->punycode->encode($uri->getHost()));
+            $uriIdnSafe = idn_to_ascii($uri->getHost());
+
+            if (false === $uriIdnSafe) {
+                throw new \InvalidArgumentException(\sprintf('Url "%s" is not valid IDN to ascii.', $url));
+            }
+
+            $uri = $uri->withHost($uriIdnSafe);
         }
 
         if (\strlen($uri->getPath()) && preg_match('/[\x80-\xff]/', $uri->getPath())) {
