90f1ac7c56bb69a720f2c54313e36ba68d1f3c05
Jan Tojnar <jtojnar@gmail.com>
2026-07-24 10:40:39 +0200

    ContentExtractorConfig: Make it readonly
    Immutability makes it easier to reason about.

--- src/Extractor/ContentExtractorConfig.php
+++ src/Extractor/ContentExtractorConfig.php
@@ -11,7 +11,7 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 /**
  * Configuration for ContentExtractor as a Value Object.
  */
-class ContentExtractorConfig
+readonly class ContentExtractorConfig
 {
     use ArrayStringOptionsTrait;
 
